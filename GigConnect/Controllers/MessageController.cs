423d3f45d0b47608c18d72b1b732d2aef49adc0e
alexHardy872 <alexhardy872@gmail.com>
2019-11-26 11:50:19 -0600

    create methode on MessagController appears to be working, now reviews and message BACKEND done, onto creating GIGS and requesting GIGS?

--- GigConnect/Controllers/MessageController.cs
+++ GigConnect/Controllers/MessageController.cs
@@ -1,7 +1,10 @@
 ﻿using GigConnect.Models;
+using GigConnect.Models.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,11 +19,96 @@ namespace GigConnect.Controllers
             context = new ApplicationDbContext();
         }
 
-        // GET: Message
-        public ActionResult Index()
+
+        // GET: Review/Create
+        public ActionResult Create(int recipientId, bool isBand)
         {
-            return View();
+            if (isBand == true)
+            {
+                MessageViewModel model = new MessageViewModel();
+                model.message = new Message();
+                model.message.timeStamp = DateTime.Now;
+                model.message.from = GetSenderType();
+                model.message.read = false;
+                model.recipientId = recipientId;
+                model.recipientType = "Band";
+                return View(model);
+            }
+            else
+            {
+                MessageViewModel model = new MessageViewModel();
+                model.message = new Message();
+                model.message.timeStamp = DateTime.Now;
+                model.message.from = GetSenderType();
+                model.message.read = false;
+                model.recipientId = recipientId;
+                model.recipientType = "Venue";
+                return View(model);
+            }
         }
+
+
+        // POST: Review/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(MessageViewModel model)
+        {
+            try
+            {
+                
+
+                if (model.message.from == "Band")
+                {
+                   model.message.bandId = GetUserBandId();
+                   model.message.venueId = model.recipientId;
+                }
+                if (model.message.from == "Venue")
+                {
+                    model.message.venueId = GetUserVenueId();
+                   model.message.bandId = model.recipientId;
+
+                }
+                context.Messages.Add(model.message);
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View("Index");
+            }
+        }
+
+
+
+        ////// helpers
+        ///
+
+        public string GetSenderType()
+        {
+            if(User.IsInRole("Band"))
+            {
+                return "Band";
+            }
+            else
+            {
+                return "Venue";
+            }
+        }
+
+        public int GetUserBandId()
+        {
+            string userId = User.Identity.GetUserId();
+            Band band = context.Bands.Where(b => b.ApplicationId == userId).FirstOrDefault();
+            return band.BandId;
+
+        }
+
+        public int GetUserVenueId()
+        {
+            string userId = User.Identity.GetUserId();
+            Venue venue = context.Venues.Where(b => b.ApplicationId == userId).FirstOrDefault();
+            return venue.VenueId;
+        }
+
 
 
 
---
+++ GigConnect/Models/ViewModels/MessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigConnect.Models.ViewModels
+{
+    public class MessageViewModel
+    {
+
+        public Message message { get; set; }
+
+        public int recipientId { get; set; }
+
+        public string recipientType { get; set; }
+    }
+}
